a429219243a98ca0f1bd8bf591575a057b227fb2
francoyupanqui <francoyupanqui25@gmail.com>
2025-04-25 22:51:58 -0500

    Start EcoRoute

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/contact" element={<h1>Contact</h1>} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
