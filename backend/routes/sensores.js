7f5094eda7adcba81e3dd9c6bfdef7993bbf3001
yohanvillarp <yohannikel21@gmail.com>
2025-06-17 21:38:14 -0500

    refactor: reestructuración del backend y mejoras de organización

--- backend/routes/sensores.js
+++
@@ -1,8 +0,0 @@
-// routes/sensores.js
-const express = require('express');
-const router = express.Router();
-const { getDatosSensores } = require('../controllers/sensoresController');
-
-router.get('/', getDatosSensores);
-
-module.exports = router;
