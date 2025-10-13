45a51c1c910de57956fd9979a7156605748281dc
yohanvillarp <yohannikel21@gmail.com>
2025-10-13 10:36:19 -0500

    refactor: actualizar App admin

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Home from "./pages/home.tsx";
+import Home from "./pages/Home.tsx";
 import LocationPage from "./pages/Locations.tsx";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
